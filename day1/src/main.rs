1282e566569722b6d0b50b9da166068690222a0c
Nora <elliemaegalyean@gmail.com>
2024-12-02 12:02:00 -0500

    day1:part2

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,14 +1,12 @@
+use std::collections::HashMap;
 use std::env;
 use std::fs::read_to_string;
 use std::path::Path;
 
-fn main() {
-    let args: Vec<String> = env::args().collect();
-    let input_file = Path::new(&args[1]);
+fn get_lists(file: &Path) -> Vec<Vec<i32>> {
     let mut lists = vec![vec![], vec![]];
-    dbg!(input_file);
-    if input_file.exists() {
-        let file = read_to_string(input_file);
+    if file.exists() {
+        let file = read_to_string(file);
         file.unwrap().lines().for_each({
             |line| {
                 let nums: Vec<&str> = line.split_whitespace().collect();
@@ -20,11 +18,33 @@ fn main() {
         });
         lists[0].sort();
         lists[1].sort();
-        let answer: i32 = lists[0]
-            .clone()
-            .into_iter()
-            .zip(lists[1].clone())
-            .fold(0, |acc, (n1, n2)| acc + (n1 - n2).abs());
-        println!("{}", answer);
     }
+    lists
+}
+
+fn get_occurances(list: Vec<i32>) -> HashMap<i32, i32> {
+    list.into_iter().fold(HashMap::new(), |mut acc, n| {
+        *acc.entry(n).or_insert(0) += 1;
+        acc
+    })
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let input_file = Path::new(&args[1]);
+    let lists = get_lists(input_file);
+    let answer1: i32 = lists[0]
+        .clone()
+        .into_iter()
+        .zip(lists[1].clone())
+        .fold(0, |acc, (n1, n2)| acc + (n1 - n2).abs());
+    println!("Part1: {}", answer1);
+    let occs = get_occurances(lists[1].clone());
+    let answer2 = lists[0]
+        .clone()
+        .into_iter()
+        .map(|n| n * occs.get(&n).unwrap_or(&0))
+        .reduce(|acc, n| acc + n)
+        .unwrap();
+    println!("Part2: {}", answer2);
 }
